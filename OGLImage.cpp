eed6a1d068835eca4faed839f0fd8d354a507a16
Sebastian <sebastian.severino@hotmail.com.ar>
2017-08-11 14:26:04 -0300

    cambiado oglwidget y terminado calculos de oglimage

--- OGLImage.cpp
+++ OGLImage.cpp
@@ -13,53 +13,51 @@ OGLImage::~OGLImage()
 void OGLImage::onEvent(OGLWidgetEvent& event)
 {
     OGLWidget::onEvent(event);
+    float width = OGLWidget::mXRight - OGLWidget::mXLeft;
+    float prop1 = width*mHeightTexture;
+    float height = OGLWidget::mYBottom - OGLWidget::mYTop;
+    float prop2 = height*mWidthTexture;
+
     if (event.type == OGL_WIDGET_SIZE) {
+        if (prop1 > prop2) {
 
-        float width = OGLWidget::mXRight - OGLWidget::mXLeft;
-        float var1 = width*mHeightTexture;
-        float height = OGLWidget::mYBottom - OGLWidget::mYTop;
-        float var2 = height*mWidthTexture;
-
-        if (var1 > var2) {
-
-            switch (mScale) {
-            case OGL_IMAGE_ADJUST:
-                mImageWidth = var2/mHeightTexture;
+            if (mScale == OGL_IMAGE_ADJUST) {
+                mImageWidth = prop2/mHeightTexture;
                 mImageX = OGLWidget::mXLeft + 0.5f*(width - mImageWidth);
-                break;
-
-            case OGL_IMAGE_FILL:
-                mImageHeight = var1/mWidthTexture;
+            } else if (mScale == OGL_IMAGE_FILL) {
+                mImageHeight = prop1/mWidthTexture;
                 mUVTextureOffset = 0.5f - 0.5f*(height/mImageHeight);
-                break;
             }
-
         } else {
 
-            switch (mScale) {
-            case OGL_IMAGE_ADJUST:
-                mImageHeight = var1/mWidthTexture;
+            if (mScale == OGL_IMAGE_ADJUST) {
+                mImageHeight = prop1/mWidthTexture;
                 mImageY = OGLWidget::mYTop + 0.5f*(height - mImageHeight);
-                break;
-
-            case OGL_IMAGE_FILL:
-                mImageWidth = var2/mHeightTexture;
+            } else if (mScale == OGL_IMAGE_FILL) {
+                mImageWidth = prop2/mHeightTexture;
                 mUVTextureOffset = 0.5f - 0.5f*(width/mImageWidth);
-                break;
             }
-
         }
+
     } else if (event.type == OGL_WIDGET_DRAW) {
 
-        switch (mScale) {
-        case OGL_IMAGE_STRETCH:
-            break;
+        if (mScale == OGL_IMAGE_STRETCH) {
+            //DRAW(OGLWidget::mXLeft, OGLWidget::mYTop, width, height, 0.0f, 0.0f, 1.0f, 1.0f);
+        } else {
+            if (prop1 > prop2) {
 
-        case OGL_IMAGE_ADJUST:
-            break;
-
-        case OGL_IMAGE_FILL:
-            break;
+                if (mScale == OGL_IMAGE_ADJUST) {
+                    //DRAW(mImageX, OGLWidget::mYTop, mImageWidth, height, 0.0f, 0.0f, 1.0f, 1.0f);
+                } else if (mScale == OGL_IMAGE_FILL) {
+                    //DRAW(OGLWidget::mXLeft, OGLWidget::mYTop, width, height, 0.0f, mUVTextureOffset, 1.0f, 1.0f - mUVTextureOffset);
+                }
+            } else {
+                if (mScale == OGL_IMAGE_ADJUST) {
+                    //DRAW(OGLWidget::mXLeft, mImageY, width, mImageHeight, 0.0f, 0.0f, 1.0f, 1.0f);
+                } else if (mScale == OGL_IMAGE_FILL) {
+                    //DRAW(OGLWidget::mXLeft, OGLWidget::mYTop, width, height, mUVTextureOffset, 0.0f, 1.0f - mUVTextureOffset, 1.0f);
+                }
+            }
         }
     }
 }
--- OGLWidget.cpp
+++ OGLWidget.cpp
@@ -4,6 +4,28 @@ OGLWidget::OGLWidget(OGLWidgetDescription description)
 {
     //FALTA CHEQUEAR CAMPOS INCOMPLETOS
     mDescription = description;
+
+    if (mDescription.x.flag == OGL_RELATIVE_LEFT) {
+        mXLeft = mDescription.x.value;
+    } else if (mDescription.x.flag == OGL_ALIGN_LEFT) {
+        mXLeft = 0.0f;
+    } else if (mDescription.x.flag == OGL_ALIGN_RIGHT) {
+        mDescription.x.value = 0;
+    }
+    if ((mDescription.x.flag == OGL_RELATIVE_LEFT || mDescription.x.flag == OGL_ALIGN_LEFT) && mDescription.width.flag == OGL_PX) {
+        mXRight = mXLeft + mDescription.width.value;
+    }
+
+    if (mDescription.y.flag == OGL_RELATIVE_TOP) {
+        mYTop = mDescription.y.value;
+    } else if (mDescription.y.flag == OGL_ALIGN_TOP) {
+        mYTop = 0.0f;
+    } else if(mDescription.y.flag == OGL_ALIGN_BOTTOM) {
+        mDescription.y.value = 0.0f;
+    }
+    if ((mDescription.y.flag == OGL_RELATIVE_TOP || mDescription.y.flag == OGL_ALIGN_TOP) && mDescription.height.flag == OGL_PX) {
+        mYBottom = mYTop + mDescription.height.value;
+    }
 }
 
 OGLWidget::~OGLWidget()
@@ -22,62 +44,74 @@ void OGLWidget::onEvent(OGLWidgetEvent& event)
         break;
 
     case OGL_WIDGET_SIZE:
-        /******************************************************************************/
-        switch (mDescription.x.flag) {
-        case OGL_ALIGN_LEFT:
-        case OGL_RELATIVE_LEFT:
-            mXLeft = (mDescription.x.flag == OGL_RELATIVE_LEFT) ? mDescription.x.value : 0.0f;
-            mXRight = mXLeft + ((mDescription.width.flag == OGL_PX) ? mDescription.width.value :
-                                //mDescription.width.value REPRESENTA UN PORCENTAJE
-                                0.01f*((OGL_WIDGET_SIZE_GET_WIDTH(event) - mXLeft)*mDescription.width.value));
-            break;
-        /******************************************************************************/
-        case OGL_ALIGN_RIGHT:
-        case OGL_RELATIVE_RIGHT:
-            //mDescription.x.value REPRESENTA LA DISTANCIA RESPECTO DEL MARGEN DERECHO EN UNIDADES DE PIXEL
-            mXRight = (mDescription.x.flag == OGL_RELATIVE_RIGHT) ? OGL_WIDGET_SIZE_GET_WIDTH(event) - mDescription.x.value :
-                      OGL_WIDGET_SIZE_GET_WIDTH(event);
-            mXLeft = mXRight - ((mDescription.width.flag == OGL_PX) ? mDescription.width.value :
-                                //mDescription.width.value REPRESENTA UN PORCENTAJE
-                                0.01f*(mXRight*mDescription.width.value));
-            break;
-        /******************************************************************************/
-        case OGL_ALIGN_CENTER_X:
-            float width = (mDescription.width.flag == OGL_PX) ? mDescription.width.value :
-                          //mDescription.width.value REPRESENTA UN PORCENTAJE
-                          0.01f*(OGL_WIDGET_SIZE_GET_WIDTH(event)*mDescription.width.value);
-            mXLeft = 0.5f*(OGL_WIDGET_SIZE_GET_WIDTH(event) - width);
-            mXRight = 0.5f*(OGL_WIDGET_SIZE_GET_WIDTH(event) + width);
-            break;
+        if (mDescription.width.flag == OGL_PX) {
+            /******************************************************************************/
+            switch (mDescription.x.flag) {
+            case OGL_ALIGN_RIGHT:
+            case OGL_RELATIVE_RIGHT:
+                mXRight = OGL_WIDGET_SIZE_GET_WIDTH(event) - mDescription.x.value;
+                mXLeft = mXRight - mDescription.width.value;
+                break;
+
+            case OGL_ALIGN_CENTER_X:
+                mXLeft = 0.5f*(OGL_WIDGET_SIZE_GET_WIDTH(event) - mDescription.width.value);
+                mXRight = 0.5f*(OGL_WIDGET_SIZE_GET_WIDTH(event) + mDescription.width.value);
+                break;
+            }
+            /******************************************************************************/
+            switch (mDescription.y.flag) {
+            case OGL_ALIGN_BOTTOM:
+            case OGL_RELATIVE_BOTTOM:
+                mYBottom = OGL_WIDGET_SIZE_GET_HEIGHT(event) - mDescription.y.value;
+                mYTop = mYBottom - mDescription.height.value;
+                break;
+
+            case OGL_ALIGN_CENTER_Y:
+                mYTop = 0.5f*(OGL_WIDGET_SIZE_GET_HEIGHT(event) - mDescription.height.value);
+                mYBottom = 0.5f*(OGL_WIDGET_SIZE_GET_HEIGHT(event) + mDescription.height.value);
+                break;
+            }
+            /******************************************************************************/
+        } else {
+            /******************************************************************************/
+            switch (mDescription.x.flag) {
+            case OGL_ALIGN_LEFT:
+            case OGL_RELATIVE_LEFT:
+                mXRight = mXLeft + 0.01f*(OGL_WIDGET_SIZE_GET_WIDTH(event) - mXLeft)*mDescription.width.value;
+                break;
+
+            case OGL_ALIGN_RIGHT:
+            case OGL_RELATIVE_RIGHT:
+                mXRight = OGL_WIDGET_SIZE_GET_WIDTH(event) - mDescription.x.value;
+                mXLeft = mXRight - 0.01f*mXRight*mDescription.width.value;
+                break;
+
+            case OGL_ALIGN_CENTER_X:
+                float width = 0.01f*OGL_WIDGET_SIZE_GET_WIDTH(event)*mDescription.width.value;
+                mXLeft = 0.5f*(OGL_WIDGET_SIZE_GET_WIDTH(event) - width);
+                mXRight = 0.5f*(OGL_WIDGET_SIZE_GET_WIDTH(event) + width);
+                break;
+            }
+            /******************************************************************************/
+            switch (mDescription.y.flag) {
+            case OGL_ALIGN_TOP:
+            case OGL_RELATIVE_TOP:
+                mYBottom = mYTop + 0.01f*(OGL_WIDGET_SIZE_GET_HEIGHT(event) - mYTop)*mDescription.height.value;
+                break;
+
+            case OGL_ALIGN_BOTTOM:
+            case OGL_RELATIVE_BOTTOM:
+                mYBottom = OGL_WIDGET_SIZE_GET_HEIGHT(event) - mDescription.y.value;
+                mYTop = mYBottom - 0.01f*mYBottom*mDescription.height.value;
+                break;
+
+            case OGL_ALIGN_CENTER_Y:
+                float height = 0.01f*OGL_WIDGET_SIZE_GET_HEIGHT(event)*mDescription.height.value;
+                mYTop = 0.5f*(OGL_WIDGET_SIZE_GET_HEIGHT(event) - height);
+                mYBottom = 0.5f*(OGL_WIDGET_SIZE_GET_HEIGHT(event) + height);
+                break;
+            }
+            /******************************************************************************/
         }
-        /******************************************************************************/
-        switch (mDescription.y.flag) {
-        case OGL_ALIGN_TOP:
-        case OGL_RELATIVE_TOP:
-            mYTop = (mDescription.y.flag == OGL_RELATIVE_TOP) ? mDescription.y.value : 0.0f;
-            mYBottom = mYTop + ((mDescription.height.flag == OGL_PX) ? mDescription.height.value :
-                                //mDescription.height.value REPRESENTA UN PORCENTAJE
-                                0.01f*((OGL_WIDGET_SIZE_GET_HEIGHT(event) - mYTop)*mDescription.height.value));
-            break;
-        /******************************************************************************/
-        case OGL_ALIGN_BOTTOM:
-        case OGL_RELATIVE_BOTTOM:
-            //mDescription.y.value REPRESENTA LA DISTANCIA RESPECTO DEL MARGEN INFERIOR EN UNIDADES DE PIXEL
-            mYBottom = (mDescription.y.flag == OGL_RELATIVE_BOTTOM) ? OGL_WIDGET_SIZE_GET_HEIGHT(event) - mDescription.y.value :
-                       OGL_WIDGET_SIZE_GET_HEIGHT(event);
-            mYTop = mYBottom - ((mDescription.height.flag == OGL_PX) ? mDescription.height.value :
-                                //mDescription.height.value REPRESENTA UN PORCENTAJE
-                                0.01f*(mYBottom*mDescription.height.value));
-            break;
-        /******************************************************************************/
-        case OGL_ALIGN_CENTER_Y:
-            float height = (mDescription.height.flag == OGL_PX) ? mDescription.height.value :
-                           //mDescription.height.value REPRESENTA UN PORCENTAJE
-                           0.01f*(OGL_WIDGET_SIZE_GET_HEIGHT(event)*mDescription.height.value);
-            mYTop = 0.5f*(OGL_WIDGET_SIZE_GET_HEIGHT(event) - height);
-            mYBottom = 0.5f*(OGL_WIDGET_SIZE_GET_HEIGHT(event) + height);
-            break;
-        }
-        /******************************************************************************/
     }
 }
