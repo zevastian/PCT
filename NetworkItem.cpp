20848118da8419b262de74c30ce5c73bcf4da636
Sebastian <sebastian.severino@hotmail.com.ar>
2017-10-28 18:59:27 -0300

    eliminado

--- NetworkItem.cpp
+++
@@ -1,9 +0,0 @@
-#include"NetworkItem.h"
-
-size_t PCTNetworkItem::onWritePrivate(char* ptr, size_t size, size_t nmemb, PCTNetworkItem* self)
-{
-    //TODO
-    //ESTA BIEN USADO STD::MOVE?
-    std::vector<char> buffer(&ptr[0], &ptr[size*nmemb]);
-    return self->onWrite(std::move(buffer)) ? size*nmemb : -1;
-}
